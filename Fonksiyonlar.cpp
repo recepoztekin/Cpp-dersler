b6153d107d2e36b245ed6164381897b9a7ad4982
Recep <103540193+recepoztekin@users.noreply.github.com>
2025-05-02 15:34:53 +0300

    Fonksiyonlar

---
+++ Fonksiyonlar.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+using namespace std;
+
+
+/*int topla(int sayi, int sayi1, int sayi2) {
+	return sayi + sayi1 + sayi2;
+}
+
+int carp(int sayi, int sayi1, int sayi2) {
+	return sayi * sayi1 * sayi2;
+}
+
+int cýkar(int sayi, int sayi1, int sayi2) {
+	return sayi - sayi1 - sayi2;
+}
+
+int bol(int sayi, int sayi1, int sayi2) {
+	return sayi - sayi1 - sayi2;
+}*/
+
+
+//---------------------------------------------
+
+	/*void kontrol(int sayi) {
+		if (sayi > 18)
+		{
+			cout << "Bu kisi Resit !!";
+		}
+		else
+		{
+			cout << "bu kisi resit degil";
+		}
+
+	}
+
+
+int main()
+{
+	int sayi;
+	cout << "Yasinizi girin: " << endl;
+	cin >> sayi;
+
+	kontrol(sayi);
+}*/
+
+
+//--------------------------------------------
+
+
+
+//void kontrol(int baslangic,int bitis) {
+//	for (int i= baslangic;i <= bitis; i++)
+//	{
+//		cout << i << endl;
+//	}
+//}
+//
+//int main() {
+//	int sayi1, sayi2;
+//	cout << "baslangic: ";
+//	cin >> sayi1;
+//	cout << "bitis: ";
+//	cin >> sayi2;
+//	kontrol(sayi1, sayi2);
+//}
+
+
+//------------------------------------------
+
+//int recep = 10; // --> global deðiþken heryerde kullanýlýr
+//
+//void fonksiyonum() {
+//	int recep = 10; // --> local deðiþken
+//     cout << recep; 
+//
+//
+//}
+//
+//
+//int main() {
+//	fonksiyonum();
+//
+//cout << recep; 
+//}
+
+//-------------------------------------------
+
+//int x = 5;
+//void fonksiyon() {
+//
+//	int x = 10;
+//	cout << x;
+//}
+//
+//int main() {
+//	fonksiyon();
+//	cout << x;
+//}
+//// Çýktý : 10 5 
+
+
+//---------------------------------------------
+
+// Fonksiyonda varsayýlan deðer Default parametresi araþtýr örnek yap
+
+/*
+int x = 5;
+
+void fonksiyon1() {
+	int x = 3;
+	cout <<"Fonksiyon 1: " << x << endl;
+}
+
+void fonksiyon2() {
+
+	int x = 15;
+	cout <<"Fonksiyýn 2: " << x << endl;
+}
+
+void fonksiyon3() {
+
+	cout <<"Fonksiyon 3: " << 2 + x + 14 << endl;
+
+}
+
+int main() {
+	fonksiyon2();
+	fonksiyon3();
+	cout <<"Globale degisken: " << x << endl;
+	fonksiyon1();
+}
+*/
+
+//-----------------------------------------------
+
+
+
+
+
+
+
